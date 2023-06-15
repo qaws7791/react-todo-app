e3f5d1788faf7ef53c0cb371bc532528e4ef2b67
qaws7791 <qaws7791@gmail.com>
2023-06-15 15:37:39 +0900

    Feat: TodoListSection__Item 위치 변경 기능 추가
    TodoListSection__Item의 각각 위치를 transform으로 동적으로 변경

--- src/components/TodoListSection/TodoListSection.jsx
+++ src/components/TodoListSection/TodoListSection.jsx
@@ -1,12 +1,47 @@
+import { useEffect, useMemo, useRef, useState } from "react";
 import TodoItem from "../TodoItem/TodoItem";
 import './TodoListSection.css';
 
 const TodoListSection = ({ title, todos, deleteTodo, updateTodo,editTodo }) => {
+  const componentRef = useRef(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      calcPositions()
+    }
+    handleResize();
+    window.addEventListener('resize',handleResize)
+    return () => {
+      window.removeEventListener('resize',handleResize)
+    }
+  })
+
+  function calcPositions() {
+    const colWidth = 360;
+    const colLength = Math.floor(componentRef.current.offsetWidth / colWidth);
+    const childElements = componentRef.current.childNodes;
+    console.log(childElements)
+    let sumWidth = 0;
+    let sumHeight = new Array(colLength).fill(0);
+    const rowGap = 20;
+    for (let i = 0; i < childElements.length; i++) {
+      childElements[i].style.transform = `translate(${sumWidth}px, ${sumHeight[i%colLength]}px)`;
+
+      sumWidth += childElements[i].offsetWidth
+      sumHeight[i%colLength] += childElements[i].offsetHeight + rowGap;
+
+      if( i % colLength === colLength-1) {
+        sumWidth = 0;
+      }  
+    }
+    componentRef.current.style.height =`${Math.max(...sumHeight)}px`
+  }
+
   return (
     <div className="TodoListSection">
       <h2 className="TodoListSection__title">{title}</h2>
-      <ul className="TodoListSection__list">
-        {todos.map((todo) => (
+      <ul className="TodoListSection__list" ref={componentRef}>
+      {todos.map((todo, index) => (
           <li key={todo.id} className="TodoListSection__Item">
           <TodoItem
             data={todo}
