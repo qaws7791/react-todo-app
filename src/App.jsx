85adbfb42a87dc0cf8e670d6fb5318755625265e
qaws7791 <qaws7791@gmail.com>
2023-06-19 14:30:57 +0900

    Feat: redux 초기 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useSelector } from "react-redux";
 import "./App.css";
 import Layout from "./Layout";
 import { TodoApp } from "./components";
 
 function App() {
+  const data = useSelector((state) => {
+    return state;
+  })
+  console.log(data)
   return (
   <Layout>
     <TodoApp />
